83c91517f1b4de9ce28cae3c79f141b5f4be408a
joy-hui <tongwandou432@gmail.com>
2023-03-16 20:31:13 +0000

    initial

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import App from './App';
-
-// converting react app to older version:
-// https://dev.to/lowla/create-react-app-with-a-previous-version-of-react-4g03
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // this dependency needed to be updated. 
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,15 +10,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-// needs to be changed to the above
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
